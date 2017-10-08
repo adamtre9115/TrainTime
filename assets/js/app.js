684415c3744b710da30c08d6a204607bf85ec516
adamtre9115 <treadam9115@gmail.com>
2017-10-08 14:06:33 -0400

    add logic for capturing input and firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,45 @@ $(document).ready(function () {
     var database = firebase.database();
 
     //   capture entered train information
-    // send it to firebase
+    $("#enterBtn").on("click", function(e){
+        e.preventDefault();
+
+        var trainName = $("#trainName").val().trim();
+        var trainDest = $("#trainDest").val().trim();
+        var trainStart = $("#trainStart").val().trim();
+        var trainFreq = $("#trainFreq").val().trim();
+
+        console.log(trainName);
+        console.log(trainDest);
+        console.log(trainStart);
+        console.log(trainFreq);
+
+
+        // send it to firebase
+        database.ref().push({
+            trainName: trainName,
+            trainDest: trainDest,
+            trainStart: trainStart,
+            trainFreq: trainFreq
+        });
+
+        // firebase event to add info to table 
+        database.ref().on("child_added", function(snapshot){
+            var trainName = snapshot.val().trainName;
+            var trainDest = snapshot.val().trainDest;
+            var trainFreq = snapshot.val().trainFreq;
+
+            console.log(snapshot.val());
+            console.log(trainName);
+            console.log(trainDest);
+            console.log(trainFreq);
+
+            //  time for frequency and next train minutes away 
+        });
+
+    });
+    //end on click function 
+    
     // calculate next train arrival relative to current time
 
 
