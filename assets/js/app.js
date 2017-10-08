3cc1bb4a664cbc16d360ab7f761df12177dacd62
adamtre9115 <treadam9115@gmail.com>
2017-10-08 12:48:14 -0400

    basic skeleton

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDJGKPVRLld2xjCPra7MsWO3_Nc2Zx793M",
+        authDomain: "trainschedule-1b60d.firebaseapp.com",
+        databaseURL: "https://trainschedule-1b60d.firebaseio.com",
+        projectId: "trainschedule-1b60d",
+        storageBucket: "",
+        messagingSenderId: "358087709153"
+    };
+    firebase.initializeApp(config);
+
+    //   variables
+    var database = firebase.database();
+
+    //   capture entered train information
+    // send it to firebase
+    // calculate next train arrival relative to current time
+
+
+
+
+});
+//end document ready
